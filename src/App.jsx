cb6a09141c98d105d9aa275bf4ea239e59cf2b53
martins <gabrielamspinola@gmail.com>
2023-01-17 20:12:16 -0300

    route update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from './Pages/Home';
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="https://martinsgabel.github.io/note-padding/" element={<Home />} />
     </Routes>
   );
 }
