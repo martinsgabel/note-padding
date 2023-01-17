311f41d156ef8e3f5d3073212e2b244ea9c5239c
martins <gabrielamspinola@gmail.com>
2023-01-17 19:44:08 -0300

    removing routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
-import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
 
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  );
+  return (<Home />);
 }
